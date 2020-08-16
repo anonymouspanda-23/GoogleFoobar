044835ee9f4008ace44049595ca912ffed79092a
anonymouspanda-23 <1702951c@student.tp.edu.sg>
2020-08-16 19:17:51 +0800

    Solved challenge

--- prisoner_locating.py
+++ prisoner_locating.py
@@ -7,10 +7,28 @@ Sample Prison Layout:
 | 2  5  9  14 20
 | 1  3  6  10 15
 
-x = 1: (n ** 2) + (n * 1) / 2
-x = 2: (n ** 2) + (n * 3) / 2
-x = 3: (n ** 2) + (n * 6) / 2 ->
-x = 4:
-x = 5:
-
 """
+
+def formula_x(y_coords):
+    return 2 * y_coords - 1
+
+
+def formula_y(y_coords):
+    return (y_coords * (y_coords - 1)) / 2
+
+
+def cell_id(coordinates):
+    x, y = coordinates
+    id = (((x ** 2) + (formula_x(y) * x)) / 2) + formula_y(y - 1)
+
+    print(f"ID of prisoner at {x}, {y} is {id}.")
+    # print(f"X number is {formula_x(y)}.")
+    # print(f"Added number is {formula_y(y - 1)}.")
+    # print("\n")
+
+
+if __name__ == '__main__':
+    cell_id((1, 1))
+    cell_id((2, 1))
+    cell_id((2, 4))
+    cell_id((5, 5))
