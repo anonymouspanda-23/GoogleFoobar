d1020d7967ff2f5abfd82f12e3a2b8eebf1cf551
anonymouspanda-23 <1702951c@student.tp.edu.sg>
2020-08-20 10:43:36 +0800

    Level 3 Question 1: Added ability to break 1 wall

--- a_star_search.py
+++ a_star_search.py
@@ -1,9 +1,8 @@
-import copy
 import time
 
 
 class Node:
-    def __init__(self, parent, position):  # todo: add attribute of wall_broken
+    def __init__(self, parent, position):
         self.g = 0
         self.h = 0
         self.f = 0
@@ -13,10 +12,7 @@ class Node:
         self.wall_broken = False
 
     def __eq__(self, other):
-        return self.position == other.position
-
-    def __repr__(self):
-        return str(self.position) + str({self.f})
+        return self.position == other.position and self.wall_broken == other.wall_broken
 
 
 def return_path(solvable, current_node):
@@ -29,12 +25,12 @@ def return_path(solvable, current_node):
 
     path = path[::-1]
 
-    return [solvable, path]
+    return solvable, path
 
 
-def astar(maze, start_pos=tuple(), end_pos=tuple()):
+def search(maze, start_pos=tuple(), end_pos=tuple()):
     """
-    astar function attempts to look for a path, returns solvable as False if there is no possible path to end state
+    search function attempts to look for a path, returns solvable as False if there is no possible path to end state
     :param maze:
     :param start_pos:
     :param end_pos:
@@ -58,10 +54,7 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
 
     # initialize start and end nodes
     start_node = Node(None, start_pos)
-    start_node.g = start_node.h = start_node.f = 0
-
     end_node = Node(None, end_pos)
-    end_node.g = end_node.h = end_node.f = 0
 
     # define open and closed lists
     open_list = []  # list of nodes to expand
@@ -72,10 +65,10 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
 
     # define legal moves
     valid_moves = [
-        [-1, 0],  # move up
-        [1, 0],  # move down
-        [0, -1],  # move left
-        [0, 1]   # move right
+        (-1, 0),  # move up
+        (1, 0),  # move down
+        (0, -1),  # move left
+        (0, 1)   # move right
     ]
 
     # initialise current_node
@@ -83,15 +76,8 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
 
     # while nodes available in open_list to expand
     while open_list:
-
-        print(f"Current Open List: {open_list}")
-        print(f"Current Closed List: {closed_list}")
-        print(f"Current Node: {current_node}")
-        input("\n")
-        
         # get 1 node from list
         current_node = open_list[0]
-        current_index = 0
 
         # check if current iteration has reached iteration limit
         if current_iter >= max_iter:
@@ -101,24 +87,22 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
         current_iter += 1
 
         # use current node to get optimal node
-        for index, node in enumerate(open_list):
-            if node.f < current_node.f:  # use h cost
+        for node in open_list:
+            if current_node.f > node.f:
                 current_node = node
-                current_index = index
-
-        if current_node == end_node:
-            return return_path(True, current_node)
 
         # remove current node from open_list and add it to closed_list
-        open_list.pop(current_index)
+        open_list.remove(current_node)
         closed_list.append(current_node)
+
+        if current_node.position == end_node.position:
+            return return_path(True, current_node)
 
         # create list to store new valid nodes expanded from current node
         children = []
 
         # loop through valid_moves and add children nodes in valid positions to children list for expanding
-        for move in valid_moves:
-
+        for move in valid_moves:  # check section
             # create child position
             child_position = (current_node.position[0] + move[0], current_node.position[1] + move[1])
 
@@ -126,8 +110,8 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
             child_node = Node(current_node, child_position)
             child_node.wall_broken = current_node.wall_broken
 
-            # check if child position is in maze
-            if not 0 <= child_position[0] <= n_rows - 1 or not 0 <= child_position[1] <= n_cols - 1:
+            # check if child position is not in maze
+            if not 0 <= child_position[0] < n_rows or not 0 <= child_position[1] < n_cols:
                 continue
 
             # check if child position is not in wall
@@ -141,62 +125,22 @@ def astar(maze, start_pos=tuple(), end_pos=tuple()):
             if child_node not in closed_list:
                 children.append(child_node)
 
-        for child_node in children:
-
-            # check if child_node is in closed_list
-            # if child_node in closed_list:
-            #     continue
-
+        for child_node in children:  # section clear
             # calculate child_node g, h and f values
             child_node.g = current_node.g + 1
-            child_node.h = abs((end_node.position[0] - child_node.position[0]) + (end_node.position[1] - child_node.position[1]))
+            child_node.h = (end_node.position[0] - child_node.position[0]) + (end_node.position[1] - child_node.position[1])
             child_node.f = child_node.g + child_node.h
 
+            # adding child_node to open_list (version 2)
             for node in open_list:
                 if child_node == node and child_node.g < node.g:
-                    node.g = child_node.g
-                    node.h = child_node.h
-                    node.f = child_node.f
-
-                    node.parent = child_node.parent
-                    node.wall_broken = child_node.wall_broken
+                    index = open_list.index(node)
+                    open_list[index] = child_node
 
             if child_node not in open_list:
                 open_list.append(child_node)
 
     return return_path(False, current_node)
-
-
-def destroy_wall(maze):  # todo: try to optimize -> https://stackoverflow.com/questions/2489672/removing-the-obstacle-that-yields-the-best-path-from-a-map-after-a-traversal
-    solvable, resultant_path = astar(maze)
-    maze_copy = copy.deepcopy(maze)
-    path_length = len(resultant_path)
-
-    for y, row in enumerate(maze_copy):
-        for x, col in enumerate(row):
-
-            if maze_copy[y][x] == 1:
-                maze_copy[y][x] = 0
-                new_solvable, new_resultant_path = astar(maze_copy)
-                new_path_length = len(new_resultant_path)
-                maze_copy[y][x] = 1
-
-                if solvable and new_solvable and new_path_length < path_length:
-                    path_length = new_path_length
-                    resultant_path = new_resultant_path
-
-                elif not solvable and new_solvable:
-                    path_length = new_path_length
-                    resultant_path = new_resultant_path
-                    solvable = new_solvable
-
-                elif solvable and not new_solvable:
-                    continue
-
-                else:  # if maze is unsolvable with or without removing wall
-                    continue
-
-    return solvable, resultant_path
 
 
 def test_algorithm(maze):
@@ -210,40 +154,16 @@ def test_algorithm(maze):
     print(f"End Point: {tuple(end_pos)}.")
 
     start_time = time.time()
-    solvable, resultant_path = astar(maze)  # function to search without removing a wall
-    # solvable, resultant_path = destroy_wall(maze)  # function to search with ability to remove 1 wall
+    solvable, resultant_path = search(maze)
     end_time = time.time()
 
     print(f"Time taken to execute: {end_time - start_time}s")
-
     print(f"Resulting path coordinates: {resultant_path}")
-
     print(f"Cost: {len(resultant_path)}")
-
-    print("\n")
-
-    print("Solved maze:") if solvable else print("Partially solved maze:")
-
-    for y, row in enumerate(maze):
-        print("")
-
-        for x, column in enumerate(row):
-            if (y, x) == tuple(start_pos):
-                print('SS', end="")
-            elif (y, x) == tuple(end_pos):
-                print('EE', end="")
-            elif (y, x) in resultant_path:
-                print('MV', end="")
-            elif maze[y][x] == 1:
-                print(u"\u2588" * 2, end="")
-            else:
-                print("[]", end="")
-
-    print("\n")
+    print(f"Solvable: {solvable}")
 
 
 if __name__ == '__main__':
-
     main_start_time = time.time()
 
     test_maze_1 = [
@@ -254,6 +174,7 @@ if __name__ == '__main__':
     ]
 
     test_algorithm(test_maze_1)
+    print("")
 
     test_maze_2 = [
         [0, 0, 0, 0, 0, 0],
@@ -265,6 +186,7 @@ if __name__ == '__main__':
     ]
 
     test_algorithm(test_maze_2)
+    print("")
 
     test_maze_3 = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -286,7 +208,6 @@ if __name__ == '__main__':
     ]
 
     test_algorithm(test_maze_3)
-
     print("")
 
     test_maze_4 = [
@@ -308,7 +229,6 @@ if __name__ == '__main__':
     ]
 
     test_algorithm(test_maze_4)
-
     print("")
 
     test_maze_5 = [
@@ -335,7 +255,17 @@ if __name__ == '__main__':
     ]
 
     test_algorithm(test_maze_5)
+    print("")
+
+    test_maze_6 = [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+    ]
+
+    test_algorithm(test_maze_6)
+    print("")
 
     main_end_time = time.time()
-
     print(f"Total runtime: {main_end_time - main_start_time}s")
