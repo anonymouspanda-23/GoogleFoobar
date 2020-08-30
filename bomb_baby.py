a72eda1fc049f9e792ece9d59f62303acd037e57
anonymouspanda-23 <1702951c@student.tp.edu.sg>
2020-08-30 17:35:31 +0800

    Level 3 Question 3

--- bomb_baby.py
+++ bomb_baby.py
@@ -8,36 +8,37 @@ def solution(x, y):
     count = 0
 
     while x > 0 and y > 0:
-        count += 1
-        if x == 1 and y == 1:
-            break
-        
         if x > y:
-            x = x - y
-        if y > x:
-            y = y - x
-        
-        print("X: " + str(x))
-        print("Y: " + str(y))
+            add = x / y - 1 if x / y > 1 else 1
+            x = x - y * add
+        elif y > x:
+            add = y / x - 1 if y / x > 1 else 1
+            y = y - x * add
+        else:
+            break
 
-    print("Count: " + str(count))
+        count += add
 
     if x == 1 and y == 1:
-        return count
+        return str(count)
     else:
         return "impossible"
 
 
 class Test(unittest.TestCase):
     def test_one(self):
-        self.assertEqual(solution('4', '7'), 4)
+        self.assertEqual(solution('4', '7'), '4')
 
     def test_two(self):
-        self.assertEqual(solution('2', '1'), 1)
+        self.assertEqual(solution('2', '1'), '1')
+
+    def test_three(self):
+        self.assertEqual(solution('2', '4'), "impossible")
+
+    def test_four(self):
+        self.assertEqual(solution('5432123456788', '5432123456789'), '5432123456788')
 
 
 if __name__ == '__main__':
-    # suite = unittest.TestLoader().loadTestsFromTestCase(Test)
-    # unittest.TextTestRunner(verbosity=2).run(suite)
-    print(solution('4', '7'))
-    print(solution('2', '1'))
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test)
+    unittest.TextTestRunner(verbosity=2).run(suite)
